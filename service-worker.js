ff01882c125c1768cf1e9022391e0d4722c7f302
Medix-Code <Medix-Code@users.noreply.github.com>
2025-06-09 09:56:16 +0000

    deploy: fa1611d276d440eb5cd9be7a4299428952231fce

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js
 
 // Nom del caché principal (incrementa la versió si canvies PRECACHE_FILES)
-const CACHE_NAME = "dieta-cache-v20250609094703"; // <-- Incrementat per reflectir canvis
+const CACHE_NAME = "dieta-cache-v20250609095616"; // <-- Incrementat per reflectir canvis
 
 // Fitxers essencials per a la funcionalitat offline inicial (App Shell)
 const PRECACHE_FILES = [
