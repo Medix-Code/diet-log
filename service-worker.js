06457407904b89defceabe441633404517591a3d
Medix-Code <Medix-Code@users.noreply.github.com>
2025-07-08 22:21:41 +0000

    deploy: ad52e5d57f4f6b0abad7252839fbd61ac14d8a6f

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // service-worker.js
 
 // Aquesta variable serà actualitzada automàticament pel teu workflow de GitHub Actions.
-const VERSION = "20250708221123"; // Un valor inicial per a desenvolupament local
+const VERSION = "20250708222141"; // Un valor inicial per a desenvolupament local
 
 const APP_SHELL_CACHE_NAME = `misdietas-app-shell-${VERSION}`;
 const DYNAMIC_CACHE_NAME = `misdietas-dynamic-${VERSION}`;
@@ -75,7 +75,6 @@ self.addEventListener("activate", (event) => {
   );
 });
 
-// --- FETCH ---
 // --- FETCH ---
 self.addEventListener("fetch", (event) => {
   const { request } = event;
