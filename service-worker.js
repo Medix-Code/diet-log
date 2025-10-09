f70ab900c95c0e7177f09537aca165ad38bb974d
Medix-Code <11797436+Medix-Code@users.noreply.github.com>
2025-10-09 11:12:22 +0200

    Test version injection with GitHub Actions workflow

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // =================================================================================
 
 // Versió actual de l'app
-const VERSION = "1.2.9";
+const VERSION = "1.2.10";
 
 // Noms dels caches amb versionat
 const CACHE_NAMES = {
