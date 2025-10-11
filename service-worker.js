d29b140b46ed8102c3cdadaaf4316d9196ca1214
Medix-Code <11797436+Medix-Code@users.noreply.github.com>
2025-10-11 21:27:44 +0200

    fix

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
 // =================================================================================
 
 // Versió actual de l'app
-const VERSION = "1.3.9";
+const VERSION = "1.4.0";
 
 // Noms dels caches amb versionat
 const CACHE_NAMES = {
