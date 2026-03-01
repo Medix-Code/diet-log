54aa44c403f1eeae9ceda0dab553d3fc3670a9f0
Medix-Code <11797436+Medix-Code@users.noreply.github.com>
2026-03-01 11:30:21 +0100

    feat: redesign help modal

--- verify-hashes.js
+++ verify-hashes.js
@@ -17,8 +17,8 @@ console.log("🔍 Verificant hashes d'integritat...\n");
 
 // Fitxers a verificar
 const files = {
-  "/dist/bundle.js?v=2.4.3": "dist/bundle.js",
-  "/css/main.min.css?v=2.4.3": "css/main.min.css",
+  "/dist/bundle.js?v=2.4.4": "dist/bundle.js",
+  "/css/main.min.css?v=2.4.4": "css/main.min.css",
 };
 
 // Service Worker path
